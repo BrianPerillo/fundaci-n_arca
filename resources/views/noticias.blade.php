8a04e3a84cab7932b997bdc5383c6a18385b5ca3
BrianPerillo <brian_perillo@hotmail.com>
2021-06-01 18:42:16 -0300

    Noticias: Cargo todo lo que faltaba, noticias y enlaces para seguir leyendo

--- resources/views/noticias.blade.php
+++ resources/views/noticias.blade.php
@@ -8,7 +8,7 @@
         <article>
             <div class="col pl-5 pr-5 pb-5">
                 <h2>Fundación ARCA realiza jornadas de salud animal en Mocoa y Puerto Asís</h2>
-                <p>28 febrero, 2014 por MiPutumayo</p>
+                <p>28 de febrero, 2014 por MiPutumayo</p>
                 <p class="mt-2">
                     La Fundación ARCA, para el Rescate y Cuidado Animal vienen organizando una serie de jornadas de salud animal en barrios de Mocoa y Puerto Asis.
                 </p>
@@ -21,7 +21,7 @@
         <article>
             <div class="col p-5">
                 <h2>Fundación ARCA le apuesta al cuidado y bienestar animal</h2>
-                <p>19 marzo, 2014 por MiPutumayo</p>
+                <p>19 de marzo, 2014 por MiPutumayo</p>
                 <p class="mt-2">
                     Los municipios de Mocoa y Puerto Asís iniciaron el 2014 con nuevas brigadas de salud animal
                     totalmente gratuitas para perros y gatos. La Fundación Arca, quien es la promotora de esta iniciativa en el departamento del Putumayo, 
@@ -29,7 +29,7 @@
                     una cultura de tenencia responsable de animales de compañía.
                 </p>
                 <div style="float: right">
-                    <a class="p-3 btn btn-primary" href="https://miputumayo.com.co/2014/02/28/fundacion-arca-realiza-jornadas-de-salud-animal-en-mocoa-y-puerto-asis/">Seguir Leyendo</a>
+                    <a class="p-3 btn btn-primary" href="https://miputumayo.com.co/2014/03/19/fundacion-arca-le-apuesta-al-cuidado-y-bienestar-animal/">Seguir Leyendo</a>
                 </div>
             </div>  
         </article>
@@ -37,13 +37,13 @@
         <article>
             <div class="col p-5">
                 <h2>Mocoa y Puerto Asís, unidos en la defensa animal</h2>
-                <p>6 octubre, 2014 por MiPutumayo</p>
+                <p>6 de octubre, 2014 por MiPutumayo</p>
                 <p class="mt-2">
                     Con la participación de centenares de personas, Putumayo realizó la II Marcha por los Derechos de los Animales organizada por la Fundación ARCA, 
                     una entidad sin ánimo de lucro que viene trabajando en el departamento hace tres años.
                 </p>
                 <div style="float: right">
-                    <a class="p-3 btn btn-primary" href="https://miputumayo.com.co/2014/02/28/fundacion-arca-realiza-jornadas-de-salud-animal-en-mocoa-y-puerto-asis/">Seguir Leyendo</a>
+                    <a class="p-3 btn btn-primary" href="https://miputumayo.com.co/2014/10/06/mocoa-y-puerto-asis-unidos-en-la-defensa-animal/">Seguir Leyendo</a>
                 </div>
             </div>  
         </article>
@@ -51,7 +51,7 @@
         <article>
             <div class="col p-5">
                 <h2>Primer Foro de Protección Animal en el Departamento del Putumayo</h2>
-                <p>23 junio, 2015 por MiPutumayo</p>
+                <p>23 de junio, 2015 por MiPutumayo</p>
                 <p class="mt-2">
                     Con gran éxito se llevó a cabo el I Foro de Protección animal en el departamento del Putumayo, realizado por la Fundación ARCA, 
                     con el apoyo de la Alcaldía de Puerto Asís en el auditorio Centenario de Comfamiliar.
@@ -60,7 +60,7 @@
                     con el objetivo de brindar herramientas para conocer e identificar la problemática que se presenta en el Departamento y las gestiones que vienen desarrollando diferentes actores en favor de los animales.
                 </p>
                 <div style="float: right">
-                    <a class="p-3 btn btn-primary" href="https://miputumayo.com.co/2014/02/28/fundacion-arca-realiza-jornadas-de-salud-animal-en-mocoa-y-puerto-asis/">Seguir Leyendo</a>
+                    <a class="p-3 btn btn-primary" href="https://miputumayo.com.co/2015/06/23/primer-foro-de-proteccion-animal-en-el-departamento-del-putumayo/">Seguir Leyendo</a>
                 </div>
             </div>  
         </article>
@@ -68,7 +68,7 @@
         <article>
             <div class="col p-5">
                 <h2>Fundación ARCA y Secretaría de Salud Departamental, unidas por la salud y protección animal</h2>
-                <p>2 mayo, 2016 por MiPutumayo</p>
+                <p>2 de mayo, 2016 por MiPutumayo</p>
                 <p class="mt-2">
                     Con una campaña denominada “Salud y Protección Animal, Una Responsabilidad de Todos”, la Fundación ARCA, dio inicio a una serie 
                     de actividades que se realizarán durante el transcurso del año 2016,  con el objetivo de seguir fortaleciendo en la comunidad temas 
@@ -76,7 +76,53 @@
                     mediante charlas y brigadas de salud en diferentes barrios del municipio de Mocoa y del Departamento.
                 </p>
                 <div style="float: right">
-                    <a class="p-3 btn btn-primary" href="https://miputumayo.com.co/2014/02/28/fundacion-arca-realiza-jornadas-de-salud-animal-en-mocoa-y-puerto-asis/">Seguir Leyendo</a>
+                    <a class="p-3 btn btn-primary" href="https://miputumayo.com.co/2016/05/02/fundacion-arca-y-secretaria-de-salud-departamental-unidas-por-la-salud-y-proteccion-animal/">Seguir Leyendo</a>
+                </div>
+            </div>  
+        </article>
+
+        <article>
+            <div class="col p-5">
+                <h2>Perritos de Mocoa, los otros afectados que pocos han atendido</h2>
+                <p>5 de Abril, 2017 por Pilar Cuartas Rodríguez</p>
+                <p class="mt-2">
+                    Cerca de 20 voluntarios se unieron para rescatar de los escombros y recoger de la calle a los perros y gatos damnificados por la avalancha en Putumayo. 
+                    Piden adecuar un albergue para estos animales y donaciones de medicamentos.
+                </p>
+                <div style="float: right">
+                    <a class="p-3 btn btn-primary" href="https://www.elespectador.com/noticias/nacional/perritos-de-mocoa-los-otros-afectados-que-pocos-han-atendido/">Seguir Leyendo</a>
+                </div>
+            </div>  
+        </article>
+
+        <article>
+            <div class="col p-5">
+                <h2>La Fundación ARCA le apuesta a la recuperación de Mocoa</h2>
+                <p>11 de Septiembre, 2017 por MiPutumayo</p>
+                <p class="mt-2">
+                    Con una campaña denominada “Salud y Protección Animal, Una Responsabilidad de Todos”, la Fundación ARCA, dio inicio a una serie 
+                    de actividades que se realizarán durante el transcurso del año 2016,  con el objetivo de seguir fortaleciendo en la comunidad temas 
+                    importantes como el bienestar de los animales de compañía, la no tenencia ilegal de fauna silvestre y el cuidado del medio ambiente,  
+                    mediante charlas y brigadas de salud en diferentes barrios del municipio de Mocoa y del Departamento.
+                </p>
+                <div style="float: right">
+                    <a class="p-3 btn btn-primary" href="https://miputumayo.com.co/2017/09/11/la-fundacion-arca-le-apuesta-a-la-recuperacion-de-mocoa/">Seguir Leyendo</a>
+                </div>
+            </div>  
+        </article>
+
+        <article>
+            <div class="col p-5">
+                <h2>Animales en el Plan Nacional del Gestión del Riesgo - Colombia</h2>
+                {{-- <p>2 mayo, 2016 por MiPutumayo</p> --}}
+                <p class="mt-2">
+                    Estimado Director:
+                    Por favor, incluya a los animales en El Plan Nacional de Gestión d​el Riesgo de Desastres de Colombia, “una Estrategia de Desarrollo”, tanto domésticos como 
+                    silvestres y medios de vida, para que puedan destinarse recursos humanos, técnicos y económicos en su bienestar y protección.
+                    La ley 1774 de 2016 los declara como seres sintientes, por lo tanto son seres que sufren durante y después de los desastres.
+                </p>
+                <div style="float: right">
+                    <a class="p-3 btn btn-primary" href="https://www.hagamoseco.org/petitions/animales-en-el-plan-nacional-del-gestion-del-riesgo-en-colombia">Seguir Leyendo</a>
                 </div>
             </div>  
         </article>
